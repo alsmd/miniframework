1c7e6518f8ec939a20f68ca9beb5ffa68cf3aa29
Celebro01 <dangerveneta@gmail.com>
2020-10-11 06:57:05 -0300

    Adicionando layou padrão

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -9,11 +9,11 @@ class IndexController extends Action{
     public function index(){
 
         $this->view->dados = array("sofa","cadeira","cama"); //podemos recuperar essa informação dentro da nossa view
-        $this->render('index');
+        $this->render('index','layout1');
     }
     public function sobreNos(){
         $this->view->dados = array("Maça","Abacate","Mamão"); //podemos recuperar essa informação dentro da nossa view
-        $this->render('sobreNos');
+        $this->render('sobreNos','layout1');
     }
     
 
--- vendor/MF/Controller/Action.php
+++ vendor/MF/Controller/Action.php
@@ -3,21 +3,34 @@
     namespace MF\Controller;
 
     abstract class Action{
-
         protected $view;
+
 
         public function __construct(){
             $this->view = new \stdClass(); //criaremos um obj vazio, para que seus atributo sejam construidos ao longo do processamento da nossa aplicação
         }
 
-        protected function render($view){
+        protected function render($page,$layout){
+            $this->view->page = $page;
+
+            if(file_exists("../App/Views/".$layout.".phtml")){
+                //renderiza nosso layout, que por sua vez ira renderizar a pagina solicitada
+                require_once "../App/Views/".$layout.".phtml";
+            }else{
+                $this->content();
+            }
+            
+        }
+
+        protected function content(){
             //recupera o primeiro nome da class atual
             $classAtual = get_class($this);
             $classAtual = str_replace("App\\Controllers\\",'',$classAtual);
             $classAtual = strtolower(str_replace("Controller","",$classAtual));
     
-            require_once "../App/Views/$classAtual /".$view.".phtml";
+            require_once "../App/Views/$classAtual /".$this->view->page.".phtml";
         }
+        
     }
 
 ?>
